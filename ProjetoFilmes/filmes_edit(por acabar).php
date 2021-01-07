ecc88dd8329887b78e33771b16859333cd8447c7
Utilizador <Utilizador@WIN-MP3JADQD58A.local>
2021-01-07 12:23:35 +0000

    atividade CRUD

---
+++ ProjetoFilmes/filmes_edit(por acabar).php
@@ -0,0 +1,44 @@
+<?php
+    if($_SERVER['REQUEST_METHOD']=="GET"){
+        if(isset($_GET['filme']) && is_numeric($_GET['filme'])){
+            $idFIlme = $_GET['filme'];
+            $con = new mysqli("localhost", "root", "filmes");
+            if($con->connect_error!=0){
+                echo "<h1>Ocorreu um erro no acesso à base de dados. <br>" .$con->connect_error. "</h1>";
+                exit();
+            }
+            $sql = "Select * from filmes where id_filme=?";
+            $stm = $con->prepare($sql);
+            if($stm!=false){
+                $stm->bind_param("i" , $idFilme);
+                $stm->execute();
+                $res=$stm->get_result();
+                $stm->close();
+            }
+?>
+        <!DOCTYPE html>
+        <html>
+            <head>
+                <meta charset="ISO-8859-1">
+                <title>Editar filme</title>
+            </head>
+            <body>
+                <h1>Editar filme</h1>
+                <form action="filmes_create.php" method="post">
+                    <label>Titulo</label><input type="text" name="titulo" required value="<?php echo $livro['titulo'];?>"><br>
+                    <label>Sínopse</label><input type="text" name="sinopse"  value="<?php echo $livro['sinopse'];?>"><br>
+                    <label>Quantidade</label><input type="text" name="quantidade"  value="<?php echo $livro['quantidade'];?>"><br>
+                    <label>Idioma</label><input type="text" name="idioma"  value="<?php echo $livro['idioma'];?>"><br>
+                    <label>Data de Lançamento</label><input type="text" name="data_lancamento"  value="<?php echo $livro['data_lancamento'];?>"><br>
+                    <input type="submit" name="enviar"><br>
+                </form>
+            </body>
+        </html>
+    <?php
+        }
+        else{
+            echo ('<h1>Houve um erro ao processar o seu pedido.<br> Dentro de segundos será reencaminhado!</h1>');
+            header("refresh:5; url=index.php");
+        } 
+    } 
+?>
---
+++ ProjetoFilmes/filmes_show.php
@@ -0,0 +1,63 @@
+<?php
+    if($_SERVER['REQUEST_METHOD']=="GET"){
+        if(!isset($_GET['filme']) || !is_numeric($GET['filme'])){
+            echo '<script>alert("Erro ao abrir livro");</script>';
+            echo 'Aguarde um momento. A reencaminhar página';
+            header("refresh:5; url=index.php");
+            exit();
+        }
+        $idFilme=$_GET['filme'];
+        $con=new mysqli("localhost", "root", "filmes");
+        if($con->connect_error!=0){
+            echo 'Ocorreu um erro no acesso à base de dados. <br>' .$con->connect_error;
+            exit;
+        }
+        else{
+            $sql = 'select * from filmes where id_filme = ?';
+            $stm = $con->prepare ( $sql);
+            if($stm!=false){
+                $stm->bind_param('i', $idFilme);
+                $stm->execute();
+                $res=$stm->get_result();
+                $livro = $res->fetch_assoc();
+                $stm->close();
+            }
+            else{
+                echo '<br>';
+                echo ($con->error);
+                echo '<br>';
+                echo "Aguarde um momento. A reencaminhar página";
+                echo '<br>';
+                echo ("refresh:5; url=index.php");
+            }
+        }
+    }
+?>
+    <!DOCTYPE html>
+    <html>
+        <head>
+            <meta charset="ISO-8859-1">
+            <title>Detalhes</title>
+        </head>
+        <body>
+            <h1>Detalhes do Filme</h1>
+            <?php
+                if (isset($livro)){
+                    echo '<br>';
+                    echo $livro['titulo'];
+                    echo '<br>';
+                    echo $livro['sinopse'];
+                    echo '<br>';
+                    echo $livro['idioma'];
+                    echo '<br>';
+                    echo $livro['quantidade'];
+                    echo '<br>';
+                    echo $livro['data_lancamento'];
+                    echo '<br>';
+                }
+                else{
+                    echo '<h2>Parece que o filme selecionado não exite. <br>Confirme a sua seleção.</h2>';
+                }
+            ?>
+        </body>
+    </html>
